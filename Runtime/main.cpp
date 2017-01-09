50af30cf040092e6644092fe148b77699f8450a4
Marek Roubíček <roubimar@fit.cvut.cz>
2017-01-10 00:05:45 +0100

    Initial Commit

---
+++ Runtime/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Runtime
+//
+//  Created by Marek Roubíček on 10.01.17.
+//  Copyright © 2017 FIT ČVUT - RUN. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
