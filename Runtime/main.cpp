7c9219a04fd8e4741e7ee7906daabb9b2f89acfb
Daniel Pina <pinadani@fit.cvut.cz>
2017-01-11 11:16:39 -0500

    x

--- Runtime/main.cpp
+++ Runtime/main.cpp
@@ -24,7 +24,7 @@ int main(int argc, char * argv[]){
         classHeap -> getClass("test/MulNumbers");
     } catch(int e) {
         if(e == 21) cout << "Out of memory exception" << endl;
-        else if( e == 25) cout << "Class file load failed. Maybe wrong Class file name?" << endl;
+        else if( e == 25) cout << "Class file load failed. Maybe wro22ng Class file name?" << endl;
         else cout << "Exception " << e << " occured" << endl;
         return 1;
     }
